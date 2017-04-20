d1963637cae3428fe0a9b896c61b83e8daed5a52
torkelso19 <torkelso19@up.edu>
2017-04-20 11:31:02 -0700

    implemented read and writing to file along with gamma lambda and alpha variables

--- AI/sperling_19_torkelso19_accuardi18.py
+++ AI/sperling_19_torkelso19_accuardi18.py
@@ -1,6 +1,7 @@
 # -*- coding: latin-1 -*-
 import random
 import sys
+import math
 
 sys.path.append("..")  # so other modules can be found in parent dir
 from Player import *
@@ -32,8 +33,26 @@ class AIPlayer(Player):
     # whether or not the playerID has been set up yet
     me_set_up = False
 
+
+    #TD Learning Variables Below
+
+    #FileName
+    filepath = "./sperling19_torkelso19_accuardi18.txt"
+
     # a list for consolidated states and their values
     state_value_list = []
+
+    #Discount Factor
+    gamma = 0.8
+
+    #Learning Rate
+    alpha = 1.0
+
+    #Eligibility Trace
+    eLambda = 1.0
+
+    #Game Counter
+    numGames = 0
 
     # __init__
     # Description: Creates a new Player
@@ -42,11 +61,17 @@ class AIPlayer(Player):
     #   inputPlayerId - The id to give the new player (int)
     ##
     def __init__(self, inputPlayerId):
-        super(AIPlayer, self).__init__(inputPlayerId, "NoahEricAi")
+        super(AIPlayer, self).__init__(inputPlayerId, "NoahErikNickAi")
         # the coordinates of the agent's food and tunnel will be stored in these
         # variables (see getMove() below)
         self.myFood = None
         self.myTunnel = None
+
+        #Try to open utility file and if doesn't exist create it
+        try:
+            self.read_states_from_file(self.filepath)
+        except IOError:
+            self.write_state_list_to_file(self.filepath)
 
     ##
     # getPlacement
@@ -480,8 +505,12 @@ class AIPlayer(Player):
     # This agent doens't learn
     #
     def registerWin(self, hasWon):
-        # method templaste, not implemented
-        pass
+        self.numGames += 1
+
+        self.write_state_list_to_file(self.filepath)
+
+        if self.alpha > 0.001:
+            self.alpha = math.pow(1.1,-0.25*self.numGames)
 
 
     ##
@@ -534,17 +563,122 @@ class AIPlayer(Player):
     ##
     def consolidate_state(self, state):
 
-        simplified_state = state
+        # food inventory numbers
+        player_food = 0
 
-        return simplified_state
+        # queen info
+        queen_health = 0
+
+        # worker info
+        worker_count = 0
+        w_distance_to_move = 0
+
+        # drone info
+        fighter_count = 0
+
+        # The AI's player ID
+        me = state.whoseTurn
+
+        # Get a reference to the player's inventory
+        my_inv = state.inventories[me]
+
+        # Gets both the player's queens
+        my_queen = getAntList(state, me, (QUEEN,))
+
+        player_food += my_inv.foodCount
+
+        food_coords = []
+
+        foods = getConstrList(state, None, (FOOD,))
+
+        # Gets a list of all of the food coords
+        for food in foods:
+            if food.coords[1] < 5:
+                food_coords.append(food.coords)
+
+        # coordinates of this AI's tunnel
+        tunnel = my_inv.getTunnels()
+        t_coords = tunnel[0].coords
+
+        # coordinates of this AI's anthill
+        ah_coords = my_inv.getAnthill().coords
+
+        # iterates through ants and scores positioning
+        for ant in my_inv.ants:
+
+            # scores queen
+            if ant.type == QUEEN:
+                continue
+
+            # scores worker to incentivize food gathering
+            elif ant.type == WORKER:
+
+                # tallies up workers
+                worker_count += 1
+
+                # if carrying, the closer to the anthill or tunnel, the better
+                if ant.carrying:
+
+                    # distance to anthill
+                    ah_dist = approxDist(ant.coords, ah_coords)
+
+                    # distance to tunnel
+                    t_dist = approxDist(ant.coords, t_coords)
+
+                    # finds closest and scores
+                    # if ant.coords == ah_coords or ant.coords == t_coords:
+                    # print("PHill")
+                    # eval += 100000000
+                    if t_dist < ah_dist:
+                        w_distance_to_move += t_dist
+                    else:
+                        w_distance_to_move += ah_dist
+
+                # if not carrying, the closer to food, the better
+                else:
+
+                    # distance to foods
+                    f1_dist = approxDist(ant.coords, food_coords[0])
+                    f2_dist = approxDist(ant.coords, food_coords[1])
+
+                    # finds closest and scores
+                    # if ant.coords == food_coords[0] or ant.coords == food_coords[1]:
+                    # print("PFood")
+                    # eval += 500
+
+                    if f1_dist < f2_dist:
+                        w_distance_to_move += f1_dist
+                    else:
+                        w_distance_to_move += f2_dist
+
+                        # the father from enemy ants, the better
+                        # eval += -5 + self.get_closest_enemy_dist(ant.coords, enemy_inv.ants)
+
+            # scores soldiers to incentivize the disruption of the enemy economy
+            else:
+                # tallies up soldiers
+                fighter_count += 1
+
+        return [queen_health, worker_count, w_distance_to_move, fighter_count, player_food]
+
+
+
+
+
+
+
+
+
+
 
     ##
+    # append_state_to_list
     #
+    # Parameters
+    #   state - Simplified version of state
+    #   utility - Value of state
     #
-    #
-    #
-    #
-    #
+    # Returns nothing
     #
     ##
     def append_state_to_list(self, state, utility):
@@ -555,8 +689,11 @@ class AIPlayer(Player):
     ##
     # write_state_list_to_file
     #
+    # Parameters
+    #   filename - file to open
     #
-    #
+    # Return
+    #   returns nothing
     ##
     def write_state_list_to_file(self, filename):
         with open(filename, "w") as f:
